b3fd551115e5b1ffe7953a57b3105c5f4332c5fd
Jeroen Peerbolte <jeroen@stijlbreuk.nl>
2019-02-18 16:40:19 +0100

    initial commit

---
+++ generator/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+module.exports = (api, options, rootOptions) => {
+    
+    api.extendPackage({
+        devDependencies: {
+            'node-sass' : '4.x',
+            'sass-loader': '7.x'
+        }
+    });
+    api.render('./template');
+    api.onCreateComplete(() => {
+        fs.writeFileSync
+    });
+}
---
+++ generator/template/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="orange">{{ version }}</div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data() {
+      return {
+        version: process.env.VUE_APP_VERSION
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+.orange {
+  color: $brand-primary;
+}
+</style>
