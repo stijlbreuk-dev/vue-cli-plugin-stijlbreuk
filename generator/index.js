6da1d9bf11d4e7d34d8c71571eeeb5ee298878b5
Jeroen Peerbolte <jeroen@stijlbreuk.nl>
2019-02-22 15:04:39 +0100

    expands base template

--- generator/index.js
+++ generator/index.js
@@ -4,12 +4,14 @@ module.exports = (api, options, rootOptions) => {
     api.extendPackage({
         devDependencies: {
             'node-sass' : '4.x',
-            'sass-loader': '7.x'
+            'sass-loader': '7.x',
+            'reset-css': '4.x'
         },
         scripts: {
             'build:staging': 'vue-cli-service build --mode staging'
         }
     });
     api.render('./template');
+    api.injectImports(api.entryFile, `import '@/components/global/_global-imports.js'`)
     api.exitLog('🙌 Stijlbreuk scaffolding completed 🙌');
 }
--- generator/template/src/App.vue
+++ generator/template/src/App.vue
@@ -1,8 +1,15 @@
 <template>
-  <div id="app" class="orange">{{ version }}</div>
+  <div id="app" class="orange">
+    <symbols></symbols>
+    <span>
+      <svg-icon :iconId="'icon_ui_info'"></svg-icon>
+      app version: {{ version }}
+    </span>
+  </div>
 </template>
 
 <script>
+  import Symbols from '@/components/svg-symbols';
   export default {
     name: 'app',
     data() {
@@ -10,10 +17,14 @@
         version: process.env.VUE_APP_VERSION
       }
     },
+    components: {
+      Symbols
+    }
   }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+@import '@/styles/app.scss';
 .orange {
   color: $brand-primary;
 }
