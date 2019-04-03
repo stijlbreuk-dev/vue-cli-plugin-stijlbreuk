6c2fe04bebebad53441d8a364e5c14e417651b3a
Nino Vrijman <ninovrijman@gmail.com>
2019-04-03 17:53:38 +0200

    Add public folder to eslint ignore, add linting scripts to package.json

--- generator/index.js
+++ generator/index.js
@@ -1,17 +1,27 @@
 const fs = require('fs');
 module.exports = (api, options, rootOptions) => {
-    
-    api.extendPackage({
-        devDependencies: {
-            'node-sass' : '4.x',
-            'sass-loader': '7.x',
-            'reset-css': '4.x'
-        },
-        scripts: {
-            'build:staging': 'vue-cli-service build --mode staging'
-        }
-    });
-    api.render('./template');
-    api.injectImports(api.entryFile, `import '@/components/global/_global-imports.js'`)
-    api.exitLog('🙌 Stijlbreuk scaffolding completed 🙌');
+
+  api.extendPackage({
+    devDependencies: {
+      'node-sass': '4.x',
+      'sass-loader': '7.x',
+      'reset-css': '4.x'
+    },
+    scripts: {
+      'build:staging': 'vue-cli-service build --mode staging',
+      'lint:eslint': 'eslint',
+      'lint:stylelint': 'stylelint',
+      'lint:markdownlint': 'markdownlint',
+      'lint:all:eslint': 'npm run lint:eslint -- --fix --ext .js,.vue .',
+      'lint:all:markdownlint': 'npm run lint:markdownlint -- --fix "docs/*.md" "*.md"',
+      'lint:dry-run:eslint': 'npm run lint:eslint -- --ext .js,.vue .',
+      'lint:dry-run:stylelint': 'npm run lint:stylelint -- "src/**/*.{vue,scss}"',
+      'lint:dry-run:markdownlint': 'npm run lint:markdownlint -- "docs/*.md" "*.md"',
+      'lint': 'run-s lint:all:*',
+      'lint:dry-run': 'run-s lint:dry-run:*'
+    }
+  });
+  api.render('./template');
+  api.injectImports(api.entryFile, `import '@/components/global/_global-imports.js'`)
+  api.exitLog('🙌 Stijlbreuk scaffolding completed 🙌');
 }
