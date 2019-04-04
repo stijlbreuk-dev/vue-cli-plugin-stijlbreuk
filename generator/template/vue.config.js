55916ec0f3c465d5c2a3c802b096321b96b1f883
Nino Vrijman <ninovrijman@gmail.com>
2019-04-04 09:32:13 +0200

    Add newline to end of vue.config and global-imports

--- generator/template/vue.config.js
+++ generator/template/vue.config.js
@@ -1,11 +1,11 @@
 process.env.VUE_APP_VERSION = require('./package.json').version;
 
 module.exports = {
-    css: {
-        loaderOptions: {
-            sass: {
-                data: '@import "@/styles/_variables.scss";'
-            }
-        }
+  css: {
+    loaderOptions: {
+      sass: {
+        data: '@import "@/styles/_variables.scss";'
+      }
     }
+  }
 };
