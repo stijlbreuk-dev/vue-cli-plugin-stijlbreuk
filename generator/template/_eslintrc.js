2935597fcb2955664cb320c362d7df66557d598e
Jeroen Peerbolte <jeroen@stijlbreuk.nl>
2020-12-22 15:11:49 +0100

    fixes issue with eslint import/extensions

--- generator/template/_eslintrc.js
+++ generator/template/_eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parserOptions: {
-    sourceType: 'script'
+    sourceType: 'script',
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#bulb-rules
@@ -11,40 +11,27 @@ module.exports = {
      * peer dependencies which are only required for use in combination with React.
      * https://www.npmjs.com/package/@vue/eslint-config-airbnb
      */
-    '@vue/airbnb'
+    '@vue/airbnb',
   ],
   rules: {
     'arrow-body-style': [
       'error',
       'as-needed',
       {
-        requireReturnForObjectLiteral: true
-      }
+        requireReturnForObjectLiteral: true,
+      },
     ],
     'array-bracket-newline': [
       'error',
       {
-        minItems: 1
-      }
+        minItems: 1,
+      },
     ],
-    'array-element-newline': [
-      'error',
-      'always'
-    ],
-    'comma-dangle': [
-      'error',
-      'never'
-    ],
-    curly: [
-      'error'
-    ],
-    'import/extensions': [
-      'never'
-    ],
-    indent: [
-      'error',
-      2
-    ],
+    'array-element-newline': ['error', 'always'],
+    'comma-dangle': ['error', 'never'],
+    curly: ['error'],
+    'import/extensions': [0],
+    indent: ['error', 2],
     'no-console': 'off',
     // Only allow 'debugger;' statements in development
     'no-debugger': process.env.NODE_ENV !== 'development' ? 'error' : 'off',
@@ -57,42 +44,37 @@ module.exports = {
       'error',
       {
         selector: 'ForInStatement',
-        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.'
+        message:
+          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
       },
       {
         selector: 'LabeledStatement',
-        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.'
+        message:
+          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
       },
       {
         selector: 'WithStatement',
-        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.'
-      }
+        message:
+          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
     ],
     'no-underscore-dangle': [
       'error',
       {
-        allow: [
-          '_this'
-        ]
-      }
+        allow: ['_this'],
+      },
     ],
     'object-curly-newline': [
       'error',
       {
         ObjectExpression: {
-          minProperties: 1
+          minProperties: 1,
         },
-        ExportDeclaration: 'always'
-      }
+        ExportDeclaration: 'always',
+      },
     ],
-    'object-property-newline': [
-      'error',
-      {}
-    ],
-    quotes: [
-      'error',
-      'single'
-    ],
+    'object-property-newline': ['error', {}],
+    quotes: ['error', 'single'],
     strict: 'off',
     'vue/component-name-in-template-casing': [
       'error',
@@ -105,17 +87,17 @@ module.exports = {
           'transition',
           'transition-group',
           'keep-alive',
-          'slot'
-        ]
-      }
+          'slot',
+        ],
+      },
     ],
     'vue/max-attributes-per-line': [
       'error',
       {
         multiline: {
-          max: 1
-        }
-      }
+          max: 1,
+        },
+      },
     ],
     'vue/html-indent': [
       'error',
@@ -125,44 +107,38 @@ module.exports = {
         baseIndent: 1,
         closeBracket: 0,
         alignAttributesVertically: true,
-        ignores: []
-      }
+        ignores: [],
+      },
     ],
     'vue/html-self-closing': [
       'error',
       {
         html: {
-          normal: 'never'
-        }
-      }
+          normal: 'never',
+        },
+      },
     ],
-    'vue/script-indent': [
-      'error'
-    ],
-    'max-len': 'off'
+    'vue/script-indent': ['error'],
+    'max-len': 'off',
   },
   overrides: [
     {
-      files: [
-        'src/store/modules/*.js'
-      ],
+      files: ['src/store/modules/*.js'],
       rules: {
         // Disabling this rules allows parameter property reassigning in VueX mutations
-        'no-param-reassign': 'off'
-      }
+        'no-param-reassign': 'off',
+      },
     },
     {
-      files: [
-        'src/**/*'
-      ],
+      files: ['src/**/*'],
       parserOptions: {
         // This will prevent errors when using 'import' in Vue SFCs
         parser: 'babel-eslint',
-        sourceType: 'module'
+        sourceType: 'module',
       },
       env: {
-        browser: true
-      }
-    }
-  ]
+        browser: true,
+      },
+    },
+  ],
 };
